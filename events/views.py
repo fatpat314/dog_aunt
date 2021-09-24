789be2f57b405b178dd5abb95e59574d2c24e939
Patrick <w.patrick.kelly@gmail.com>
2021-09-24 16:39:49 -0700

    calander and navbar  implemented

--- events/views.py
+++ events/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "John"
+    month = month.capitalize()
+    # Convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # get current time
+    time = now.strftime('%I:%M %p')
+
+    return render(request, 
+        'events/home.html', {
+            "name": name,
+            "year": year,
+            "month": month,
+            "month_number": month_number,
+            "cal": cal,
+            "current_year": current_year,
+            "time": time,
+            })
