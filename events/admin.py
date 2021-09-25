ee8191665346fe66f4002c3015698f26835186c1
Patrick <w.patrick.kelly@gmail.com>
2021-09-24 17:23:02 -0700

    created models for events, users, and venues

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Venue
+from .models import DogAuntUser
+from .models import Event
 
-# Register your models here.
+admin.site.register(Venue)
+admin.site.register(DogAuntUser)
+admin.site.register(Event)
