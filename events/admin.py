ac33d239cb5a5add1c11ba1da1f42849fafbc10a
Patrick <w.patrick.kelly@gmail.com>
2021-09-25 09:02:02 -0700

    edited admin panel, added venue form

--- events/admin.py
+++ events/admin.py
@@ -3,6 +3,19 @@ from .models import Venue
 from .models import DogAuntUser
 from .models import Event
 
-admin.site.register(Venue)
+# admin.site.register(Venue)
 admin.site.register(DogAuntUser)
-admin.site.register(Event)
+# admin.site.register(Event)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')
+    list_filter = ('event_date', 'venue')
+    ordering = ('event_date',)
